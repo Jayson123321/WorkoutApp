23c83c96bdbe874861fadafc5d167753ce89b5c4
Jayson <jayson.haverkamp@hva.nl>
2025-03-29 15:47:44 +0100

    Added empty screens for NavHost In MainActivity. Screens: - WorkoutHistoryScreen - WorkoutAddScreen - WorkoutFavouriteScreen - WorkoutDetailScreen

---
+++ app/src/main/java/com/example/capstoneworkoutapp/ui/screens/WorkoutAddScreen.kt
@@ -0,0 +1,10 @@
+package com.example.capstoneworkoutapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun WorkoutAddScreen(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/com/example/capstoneworkoutapp/ui/screens/WorkoutScreens.kt
@@ -0,0 +1,8 @@
+package com.example.capstoneworkoutapp.ui.screens
+
+enum class WorkoutScreens {
+    WorkoutAddScreens,
+    WorkoutDetailScreen,
+    WorkoutFavouriteScreen,
+    WorkoutHistoryScreen
+}
